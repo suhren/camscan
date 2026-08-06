ca5a98ab656f752df1e94c75b5fe61d4d1896931
Adam Suhren Gustafsson <adam.suhren@gmail.com>
2026-08-06 16:18:26 +0200

    Added development check script

---
+++ scripts/check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "======== Running ruff ========"
+uv run ruff check --fix
+echo "======== Running mypy ========"
+uv run mypy .
+echo "======== Running pydoclint ========"
+uv run pydoclint .
+echo "======== Running pytest ========"
+uv run -m pytest .
