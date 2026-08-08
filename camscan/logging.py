e4af41123d8e0b3e72b02a0c83e06f665287856f
Adam Suhren Gustafsson <adam.suhren@gmail.com>
2026-08-08 12:52:55 +0200

    Refactored and improved camera handling

--- camscan/logging.py
+++ camscan/logging.py
@@ -1,14 +1,14 @@
 import logging
 
 _LOG_DATEFMT = "%Y-%m-%d %H:%M:%S"
-_LOG_FMT = "%(asctime)s.%(msecs)03d [%(levelname)s]: %(message)s"     
+_LOG_FMT = "%(asctime)s.%(msecs)03d [%(levelname)s]: %(message)s"
 _LOG_LEVEL = logging.DEBUG
 
 _formatter = logging.Formatter(fmt=_LOG_FMT, datefmt=_LOG_DATEFMT)
 
 _stream_handler = logging.StreamHandler()
-_stream_handler.setLevel(_LOG_LEVEL)
 _stream_handler.setFormatter(_formatter)
 
 logger = logging.getLogger(__name__)
 logger.addHandler(_stream_handler)
+logger.setLevel(_LOG_LEVEL)
